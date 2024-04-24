3755ea595fe46ffca94846a201418467e8cf25c3
Rio Saputro <rio.ae23@gmail.com>
2024-04-24 18:38:24 +0700

    Scriptable Object for Interaction

---
+++ Assets/Code/SOs/InteractionObject.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Interaction", menuName = "Interaction", order = 0)]
+public class InteractionObject : ScriptableObject {
+    public string objectName;
+    public string objectDescription;
+
+    [System.Serializable]
+    public class Interaction
+    {
+         public string speakerName;
+        [TextArea(5, 10)]
+        public string paragraphs;
+    }
+
+    public List<Interaction> interactions = new List<Interaction>();
+
+}
