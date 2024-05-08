32dfaf63d72a3dc02fd3e80a65d66172691241e4
Rio Saputro <rio.ae23@gmail.com>
2024-05-08 22:17:18 +0700

    Turn player facing direction

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
     private bool isSprint;
     private GameplayMaster gameplayMaster;
     private bool canMove;
+    public bool facingRight = true;
 
     
     [Header("Player Animation")]
@@ -79,9 +80,25 @@ public class PlayerController : MonoBehaviour
     {
         // Debug.Log("Player Movement");
         float horizontalMove = Input.GetAxis("Horizontal");
+        if (horizontalMove > 0 && !facingRight)
+        {
+            TurnPlayer();
+        }
+        else if (horizontalMove < 0 && facingRight)
+        {
+            TurnPlayer();
+        }
         isWalk = horizontalMove != 0 ? true : false;
         isSprint = Input.GetKey(KeyCode.LeftShift) ? true : false;
         PLAYER_SPEED = isSprint ? SPRINT_SPEED : WALK_SPEED;
         rb.velocity = new Vector2(horizontalMove * PLAYER_SPEED, rb.velocity.y);
+    }
+
+    void TurnPlayer()
+    {
+        facingRight = !facingRight;
+        Vector3 playerScale = transform.localScale;
+        playerScale.x *= -1;
+        transform.localScale = playerScale;
     }
 }
